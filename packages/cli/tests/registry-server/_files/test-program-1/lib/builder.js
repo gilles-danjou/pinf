c49a936dd669b1eabf4da95386814b071c106e5e
cadorn <christoph@christophdorn.com>
2010-01-19 19:20:13 -0800

    sync from dev copy

--- packages/cli/tests/registry-server/_files/test-program-1/lib/builder.js
+++ packages/cli/tests/registry-server/_files/test-program-1/lib/builder.js
@@ -4,10 +4,10 @@ function dump(obj) { print(require('test/jsdump').jsDump.parse(obj)) };
 
 var BUILDER = require("builder", "http://registry.pinf.org/cadorn.org/github/pinf/packages/common/");
 
-var Builder = exports.Builder = function(pkg) {
+var Builder = exports.Builder = function(pkg, options) {
     if (!(this instanceof exports.Builder))
-        return new exports.Builder(pkg);
-    this.construct(pkg);
+        return new exports.Builder(pkg, options);
+    this.construct(pkg, options);
 }
 
 Builder.prototype = BUILDER.Builder();
