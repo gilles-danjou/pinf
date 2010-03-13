31573f6a4874f4ff51a26f8b43f04f0bde3b529d
cadorn <christoph@christophdorn.com>
2010-03-13 14:34:08 -0800

    simple preprocessor for deprecated annotations

---
+++ packages/common/lib/preprocessor.js
@@ -0,0 +1,40 @@
+
+var AnnotationParser = exports.AnnotationParser = function() {
+    if (!(this instanceof exports.AnnotationParser))
+        return new exports.AnnotationParser();
+}
+
+AnnotationParser.prototype.process = function(text, path) {
+    
+    // Look for:
+    //
+    //   / * *
+    //    * @deprecated
+    //    * /
+    //   X.prototype.Y = function(...) {
+    //
+    // and insert deprecation log message
+    
+    
+    var re = /\/\*\*\s*\n(.*?)\s*\*\s*@deprecated\s*\n(.*?)\s*\*\/\s*\n(.*?\s*function\s*\(.*?\))(\s*\n?\s*\{)/g,
+        m;
+
+    while(m = re.exec(text)) {
+        text = text.replace(m[3]+m[4], m[3]+m[4] + 
+            "system.log.warn('Calling DEPRECATED function: "+m[3]+" declared in "+path+"');"
+        );
+    }
+
+    return text;
+}
+
+
+// register the preprocessor
+var processor = {
+    annotationParser: exports.AnnotationParser(),
+    process: function(text, topId, path) {
+        text = this.annotationParser.process(text, path);
+        return text;
+    }
+};
+require.loader.preprocessors.unshift(processor);
